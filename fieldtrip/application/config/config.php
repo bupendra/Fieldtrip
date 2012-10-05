2f720f55d55470643edcd87442e4dba521d1d94c
Upendra <upendranath.b@gmail.com>
2012-10-05 10:53:01 +0530

    commit add user/search ogr confg

--- fieldtrip/application/config/config.php
+++ fieldtrip/application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/fieldtrip_git/SugarCE650/fieldtrip/';
-//$config['base_url']	= 'http://localhost/fieldtrip_git/SugarCRM/fieldtrip/';
+//$config['base_url']	= 'http://localhost/fieldtrip_git/SugarCE650/fieldtrip/';
+$config['base_url']	= 'http://localhost/fieldtrip_git/SugarCRM/fieldtrip/';
 
 /*
 |--------------------------------------------------------------------------
